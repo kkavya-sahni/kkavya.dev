1d07182b274f9aed0d6f1d21b27d20af257b47ae
kkavya-sahni <lucipher.kkavya2005@gmail.com>
2024-02-16 20:01:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function hover1(){
+    document.getElementById('nav_btn1').style.backgroundColor="blue";
+    document.getElementById('nav_btn1').style.textDecoration="underline";
+}
+function hover2(){
+    document.getElementById('nav_btn2').style.backgroundColor="blue";
+    document.getElementById('nav_btn2').style.textDecoration="underline";
+}
+function hover3(){
+    document.getElementById('nav_btn3').style.backgroundColor="blue";
+    document.getElementById('nav_btn3').style.textDecoration="underline";
+}
+function hover4(){
+    document.getElementById('nav_btn4').style.backgroundColor="blue";
+    document.getElementById('nav_btn4').style.textDecoration="underline";
+}
+function hover5(){
+    document.getElementById('nav_btn5').style.backgroundColor="blue";
+    document.getElementById('nav_btn5').style.textDecoration="underline";
+}
